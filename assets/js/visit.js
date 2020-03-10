cee6fc8bf2ba13dc57b30467be3ed360b6fb0a98
gongjie <gong.jie@cn.ricoh.com>
2020-03-10 14:46:21 +0800

    test

--- assets/js/visit.js
+++ assets/js/visit.js
@@ -1,21 +1,27 @@
 
 $(document).ready(function () {
-    const soho = "http://pv.sohu.com/cityjson?ie=utf-8";
-    $.get(soho, function(res){
-        console.log(res)
-    })
     console.log(navigator, 'navigator')
 
-    console.log(returnCitySN["cip"], 'returnCitySN["cip"]')
-
+     //获取城市ajax
     $.ajax({
         url: 'http://api.map.baidu.com/location/ip?ak=ia6HfFL660Bvh43exmH9LrI6',
         type: 'POST',
         dataType: 'jsonp',
         success:function(data) {
+            console.log(data, 'data')
             console.log(data.content.address_detail.province, 'data.content.address_detail.province')
             console.log(data.content.address_detail.city, 'data.content.address_detail.city')
         }
+    });
+
+     //获取ip ajax
+     $.ajax({
+        url: 'http://freegeoip.net/json/',
+        success: function(data){
+            console.log(data, 'data freegeoip')
+        },
+        type: 'GET',
+        dataType: 'JSON'
     });
 
 
