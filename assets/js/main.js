34ebf695ef3a60951106496e081790c83b2e2881
gongjie <gong.jie@cn.ricoh.com>
2020-03-10 11:45:04 +0800

    delete fetch.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,12 +30,8 @@ var iUp = (function () {
 $(document).ready(function () {
 
 	// 获取一言数据
-	fetch('https://v1.hitokoto.cn').then(function (res) {
-		return res.json();
-	}).then(function (e) {
+	$.get('https://v1.hitokoto.cn').then(function (e) {
 		$('#description').html(e.hitokoto + "<br/> -「<strong>" + e.from + "</strong>」")
-	}).catch(function (err) {
-		console.error(err);
 	})
 
 	/**
